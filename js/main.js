0d3eb8955c5cfee0b1a139c4cf5e56325591e6e1
Valeriya <greenglaz2011@gmail.com>
2021-09-26 22:28:13 +0400

    Fixes after review

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 /*Получение случайного целого числа в заданном интервале, включительно
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
 
-const getRandomIntInclusive = (min, max) => {
-  if (min >= max ||
-    min < 0) {
+const getRandomIntInclusive = (lower, upper) => {
+
+  if (lower >= upper || lower < 0) {
     throw new Error('Некорректный диапазон чисел');
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  const min = Math.ceil(lower);
+  const max = Math.floor(upper);
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
   return result;
@@ -19,10 +19,9 @@ getRandomIntInclusive(2, 10); //временный вызов функции
 /*Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed*/
 
-const getRandomFloat = (min, max, precision) => {
-  if (min >= max ||
-    min < 0 ||
-    precision === 0) {
+const getRandomFloat = (min, max, precision = 2) => {
+
+  if (min >= max || min < 0) {
     throw new Error('Некорректный диапазон чисел');
   }
 
