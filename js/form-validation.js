7101cbb122d764f0f187bc180513961030d564aa
Valeriya <greenglaz2011@gmail.com>
2021-10-28 12:16:30 +0400

    Fixes for form-validation.js after review

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,7 +2,6 @@
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
 const MAX_PRICE = 1000000;
-const MIN_ROOMS = 1;
 const MAX_ROOMS = 100;
 
 const priceType = {
@@ -21,8 +20,8 @@ const formCapacity = adForm.querySelector('#capacity');
 const formRooms = adForm.querySelector('#room_number');
 
 const onTitleInput = () => {
-  const valueLength = formTitle.value.lenght;
-  if (valueLength === '0') {
+  const valueLength = formTitle.value.length;
+  if (valueLength === 0) {
     formTitle.setCustomValidity('Минимальная длина заголовка - 30 символов!');
   } else if (valueLength < MIN_TITLE_LENGTH) {
     formTitle.setCustomValidity(`Ещё ${  MIN_TITLE_LENGTH - valueLength } симв.`);
@@ -31,6 +30,7 @@ const onTitleInput = () => {
   } else {
     formTitle.setCustomValidity('');
   }
+  formTitle.reportValidity();
 };
 
 const changeMinPrice = () => {
@@ -38,44 +38,55 @@ const changeMinPrice = () => {
   formPrice.placeholder = priceType[formType.value];
 };
 
+const onChangePrice = () => changeMinPrice();
+
 const onPriceInput = (evt) => {
   const value = evt.target.value;
   const typeValue = priceType[formType.value];
 
-  if (value.length === '0') {
-    formPrice.setCustomValidity(`Минимальная цена ${typeValue}`);
-  } else if (value < typeValue) {
+  if (value < typeValue) {
     formPrice.setCustomValidity(`Минимальная цена ${typeValue}`);
   } else if (value > MAX_PRICE) {
     formPrice.setCustomValidity(`Максимальная цена ${MAX_PRICE}`);
   } else {
     formPrice.setCustomValidity('');
   }
+  formPrice.reportValidity();
 };
 
 const checkCapacity = () => {
-  const roomsValue = Number(formRooms.value);
-  const capacityValue = Number(formCapacity.value);
+  const rooms = Number(formRooms.value);
+  const guests = Number(formCapacity.value);
 
-  if (roomsValue === MIN_ROOMS && capacityValue === '0'
-    || roomsValue === MIN_ROOMS && roomsValue < capacityValue) {
-    formCapacity.setCustomValidity('Допустимое количество гостей 1');
-  } else if (roomsValue !== MAX_ROOMS && capacityValue === '0'
-    || roomsValue !== MAX_ROOMS && roomsValue < capacityValue) {
-    formCapacity.setCustomValidity(`Допустимое количество гостей от 1 до ${roomsValue}`);
-  } else if (roomsValue === MAX_ROOMS && capacityValue !== '0') {
-    formCapacity.setCustomValidity('Не для гостей');
+  if (rooms < guests) {
+    formCapacity.setCustomValidity('Выберите другой вариант');
+  } else if (rooms === MAX_ROOMS && guests !== 0) {
+    formCapacity.setCustomValidity('Выберите другой вариант');
+  } else if (guests === 0 && rooms !== MAX_ROOMS) {
+    formCapacity.setCustomValidity('Выберите другой вариант');
   } else {
     formCapacity.setCustomValidity('');
   }
+  formCapacity.reportValidity();
+};
+
+const onChangeCapacity = () => checkCapacity();
+
+const syncFields = () => {
+  changeMinPrice();
+  checkCapacity();
 };
 
 export const validateForm = () => {
+  syncFields();
+
   formTitle.addEventListener('invalid', onTitleInput);
   formTitle.addEventListener('input', onTitleInput);
-  formType.addEventListener('change', changeMinPrice);
+
+  formType.addEventListener('change', onChangePrice);
   formPrice.addEventListener('invalid', onPriceInput);
   formPrice.addEventListener('input', onPriceInput);
-  formRooms.addEventListener('change', checkCapacity);
-  formCapacity.addEventListener('change', checkCapacity);
+
+  formRooms.addEventListener('change', onChangeCapacity);
+  formCapacity.addEventListener('change', onChangeCapacity);
 };
